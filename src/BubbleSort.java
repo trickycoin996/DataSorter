d542f8332f1bc2f9b6e892ee757ed74756a0feaa
Dulanjan Sithmina <dulanjansithmina@gmail.com>
2025-11-07 19:33:20 +0530

    Create BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class BubbleSort {
-    
+    public static SortResult sort(ArrayList<Integer> data) {
+        ArrayList<Integer> arr = new ArrayList<>(data);
+        int steps = 0;
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            for (int j = 0; j < arr.size() - i - 1; j++) {
+                steps++;
+                if (arr.get(j) > arr.get(j + 1)) {
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                    steps++;
+                }
+            }
+        }
+
+        long endTime = System.nanoTime();
+        double elapsedTime = (endTime - startTime) / 1_000_000.0; // ms
+
+        return new SortResult(arr, steps, elapsedTime);
+    }
 }
